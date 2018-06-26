8ac595525a9e817c694ad46f70f9a6479058ef9d
Daniel Lau <daniellau@Daniels-MacBook-Pro-2.local>
2018-06-26 15:11:55 +0100

    added named-cat path that takes params

--- intro-to-the-web/app.rb
+++ intro-to-the-web/app.rb
@@ -8,7 +8,15 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
    erb(:index)
+end
+
+get '/named-cat' do
+ p params
+  @name = params[:name]
+  @color =params[:color]
+   erb(:index)
+
 end
